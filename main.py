c4ef77e5e01d27db3cfd7c9955746742993211b8
X-MAS <al.craciun98@gmail.com>
2026-02-19 18:47:20 +0200

    notes

---
+++ main.py
@@ -0,0 +1,5 @@
+# Add residents association tax a utility type
+# Utilities cost breakdown from landlord dashboard doesn't include all types and it seems a bit crowded (it's always for all properites and a bit small idk)... same goes for Business performance... try to improve these two views
+# View History from properties doesn't do anything in the popup
+# in tenants tab in the table view contract/lease period "2023-01-01 | 2024-01-01" is displayed weird when the page is smaller (the text wraps in rows but dates are split (they shouldn't, try making a small container with 2 rows"
+# landlord settings page has white text on white background in all fields
