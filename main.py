cdf2fc07ed7fc434252e505714538cdc143d5122
X-MAS <al.craciun98@gmail.com>
2026-02-19 19:18:32 +0200

    Base db

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # View History from properties doesn't do anything in the popup
 # in tenants tab in the table view contract/lease period "2023-01-01 | 2024-01-01" is displayed weird when the page is smaller (the text wraps in rows but dates are split (they shouldn't, try making a small container with 2 rows"
 # landlord settings page has white text on white background in all fields
+import os
 import subprocess
 import threading
 import sys
@@ -23,6 +24,7 @@ def main():
 
     # Start API as a separate process so it runs independently from npm
     python = sys.executable
+    print(os.getcwd())
     api_cmd = [
         python,
         "-m",
